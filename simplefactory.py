1ea6cd3a984ef6f1a359d6187afcab6a142f7e24
clorieanne <clorieanne@gmail.com>
2014-12-04 09:18:50 +0800

    created factory method and abstract factory

---
+++ simplefactory.py
@@ -0,0 +1,27 @@
+from connector import *
+import abc
+import urllib2
+from BeautifulSoup import BeautifulStoneSoup
+
+
+class SimpleFactory(object):
+  @staticmethod 
+  # This decorator allows to run method without              
+  # class instance, .e. SimpleFactory.build_connection
+  def build_connection(protocol):
+
+    if protocol == 'http':
+      return HTTPConnection()
+
+    elif protocol == 'ftp':
+      return FTPConnection()
+
+    else:
+      raise RuntimeError('Unknown protocol')
+
+if __name__ == '__main__':
+  protocol = raw_input('Which Protocol to use? (http or ftp): ')
+  protocol = SimpleFactory.build_connection(protocol)
+  protocol.connect()
+  print protocol.get_response()
+
